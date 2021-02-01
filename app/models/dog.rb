136dd4168d2534283bb1a6288c90322ec4b2095c
oopanpan <oopanpan@gmail.com>
2021-01-31 22:42:55 -0800

    commit tree e7f0df790791ab6ba2acd43551c6736c30f0718e

---
+++ app/models/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    attr_accessor :name, :breed, :age
+
+    def initialize(name, breed, age)
+        @name = name
+        @breed = breed
+        @age = age
+        @@all.push(self)
+    end
+end
